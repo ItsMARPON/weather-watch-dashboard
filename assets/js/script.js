e2ab540b0e57b24e4b6be415243a908d22686b45
Mary <itsmaryyang@gmail.com>
2023-01-24 18:35:19 -0600

    Add changes JavaScript to get URL and function to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,35 @@
+//Website Reference: https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
 // Take user input for City Name (Location) and save to local storage and print to console.
 $(document).ready(function () {
-  let apiUrl = " http://api.openweathermap.org/geo/1.0/direct?q=";
-  let limit = "&limit=5";
-  let APIKey = "&appid=4f37af9ace93cd2c4a4e0290356285bd";
-  let cityName = "Minneapolis";
-  let requestCityUrl = apiUrl + cityName + limit + APIKey;
-
-  let apiUrl2 = "api.openweathermap.org/data/2.5/forecast?";
-  let lat = "lat=44.34";
-  let lon = "&lon=10.99";
-  //   let longitude = "lat=" + listLonItem;
-  //   let latitude = "&lon=" + listLatItem;
-
-  let requestDataUrl = apiUrl2 + lat + lon + APIKey;
-
   let mainList = document.querySelector("ul");
   let cityEl = document.querySelector("#city");
-
-  //  reference: http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
 
   // Get date and time
   let today = dayjs();
   $("#currentDay").text(today.format("MMMM D, YYYY HH:mm A"));
 
-  //   Get user Input for City and set to Local storage
+  // Get user Input for City and set to Local storage
   function getCityInput() {
     $("#submit-btn").on("click", function (event) {
       event.preventDefault();
-      let cityName = cityEl.val();
+      let cityName = $("#city").val();
       localStorage.setItem("city", JSON.stringify(cityName));
-      console.log(cityName);
-      return cityName;
+      let city = localStorage.getItem("city") || [];
+      city = JSON.parse(city);
+      getCityOrGeo();
     });
   }
   getCityInput();
 
-  //   function displayCityInput() {
-  //     let cityName = getCityInput();
-  //     let city = localStorage.getItem("city") || [];
-  //     city = JSON.parse(city);
-
-  //     cityEl.textContent = "";
-  //     let cityText = cityFormEl;
-  //     cityText.value = "";
-
-  //     console.log("This finally worked");
-  //   }
-  //   displayCityInput();
-
   // Call API to get weather information
 
   function getCityOrGeo() {
+    let apiUrl = " https://api.openweathermap.org/geo/1.0/direct?q=";
+    let limit = "&limit=5";
+    let APIKey = "&appid=4f37af9ace93cd2c4a4e0290356285bd";
+    // let city = "Minneapolis";
+    let requestCityUrl = apiUrl + city + limit + APIKey;
+
     fetch(requestCityUrl)
       .then(function (response) {
         if (response.ok) {
@@ -78,35 +57,43 @@ $(document).ready(function () {
         }
       });
   }
-  getCityOrGeo();
 
-  function getWeatherData() {
-    fetch(requestDataUrl)
-      .then(function (response) {
-        if (response.ok) {
-          console.log(response);
-          return response.json();
-        } else {
-          alert("Error2:" + response.statusText);
-        }
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          var listMainItem = document.createElement("li");
-          listMainItem.textContent = data[i].main;
-          mainList.appendChild(listMainItem);
-          var listWeatherItem = document.createElement("li");
-          listWeatherItem.textContent = data[i].weather;
-          mainList.appendChild(listWeatherItem);
-          var listCloudItem = document.createElement("li");
-          listCloudItem.textContent = data[i].cloud;
-          mainList.appendChild(listCloudItem);
-          var listWindItem = document.createElement("li");
-          listWindItem.textContent = data[i].wind;
-          mainList.appendChild(listWindItem);
-          console.log("It finally worked");
-        }
-      });
-  }
-  getWeatherData();
+  //   function getWeatherData() {
+  //     let apiUrl2 = "https://api.openweathermap.org/data/2.5/forecast?";
+  //     let lat = "lat=44.34";
+  //     let lon = "&lon=10.99";
+  //     let APIKey = "&appid=4f37af9ace93cd2c4a4e0290356285bd";
+  //     //   let longitude = "lat=" + listLonItem;
+  //     //   let latitude = "&lon=" + listLatItem;
+
+  //     let requestDataUrl = apiUrl2 + lat + lon + APIKey;
+
+  //     fetch(requestDataUrl)
+  //       .then(function (response) {
+  //         if (response.ok) {
+  //           console.log(response);
+  //           return response.json();
+  //         } else {
+  //           alert("Error2:" + response.statusText);
+  //         }
+  //       })
+  //       .then(function (data) {
+  //         for (var i = 0; i < data.length; i++) {
+  //           var listMainItem = document.createElement("li");
+  //           listMainItem.textContent = data[i].main;
+  //           mainList.appendChild(listMainItem);
+  //           var listWeatherItem = document.createElement("li");
+  //           listWeatherItem.textContent = data[i].weather;
+  //           mainList.appendChild(listWeatherItem);
+  //           var listCloudItem = document.createElement("li");
+  //           listCloudItem.textContent = data[i].cloud;
+  //           mainList.appendChild(listCloudItem);
+  //           var listWindItem = document.createElement("li");
+  //           listWindItem.textContent = data[i].wind;
+  //           mainList.appendChild(listWindItem);
+  //           console.log("It finally worked");
+  //         }
+  //       });
+  //   }
+  //   getWeatherData();
 });
